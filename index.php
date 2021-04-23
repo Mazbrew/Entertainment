cb320f71a203c000fb5ab8e5cad82f7086db9b3c
mazbrew <peteryeoh50@gmail.com>
2021-04-23 14:14:29 +0800

    moved css to a seperate file and made the index page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" type="text/css" media="screen" href="stylesheet.css">
+    </head>
+
+    <body>
+        <div class = "header">
+            POWERPUFFGIRLS&BOYS
+        </div>
+
+        <div>
+            <a href= "actor.php">actor</a>
+        </div>    
+    </body>
+</html>
