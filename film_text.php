fa3affac4229abb6ad2d3b9275230c199943bdd7
aluxljy <aluxljy@gmail.com>
2021-04-25 03:16:44 +0800

    error msg

--- film_text.php
+++ film_text.php
@@ -96,12 +96,12 @@
                                 echo("<meta http-equiv='refresh' content='1'>");
                             }
                             else{
-                                echo '<script> alert("PREVIOUS INSERT FAILED")</script>';
+                                echo '<script> alert("PREVIOUS INSERT FAILED! CHECK IF THERE WERE MISTAKES MADE WHEN INSERTING")</script>';
                             }
                                 
                         } 
                         else{
-                            echo '<script> alert("FILL ALL FIELDS")</script>';
+                            echo '<script> alert("PREVIOUS INSERT FAILED, PLEASE FILL ALL FIELDS!")</script>';
                         }
                     }
                     
@@ -138,11 +138,11 @@
                                 
                                 echo("<meta http-equiv='refresh' content='1'>");
                             }else{
-                                echo '<script> alert("PREVIOUS UPDATE FAILED")</script>';
+                                echo '<script> alert("PREVIOUS UPDATE FAILED! CHECK IF THERE WERE MISTAKES MADE WHEN UPDATING")</script>';
                             }
                                 
                         }else{
-                            echo '<script> alert("FILL ALL FIELDS")</script>';
+                            echo '<script> alert("PREVIOUS UPDATE FAILED, PLEASE FILL ALL FIELDS!")</script>';
                         }
                     }
                     ?>
@@ -175,11 +175,11 @@
                             
                             echo("<meta http-equiv='refresh' content='1'>");
                         }else{
-                            echo '<script> alert("PREVIOUS DELETE FAILED")</script>';
+                            echo '<script> alert("PREVIOUS DELETE FAILED! YOU ARE NOT ALLOWED TO DELETE THIS ROW")</script>';
                         }
                             
                     }else{
-                        echo '<script> alert("FILL ALL FIELDS")</script>';
+                        echo '<script> alert("PREVIOUS DELETE FAILED, PLEASE FILL ALL FIELDS!")</script>';
                     }
                 }
             ?>
