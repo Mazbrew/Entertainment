04b1d419ef0a6c44865ec060b97e57b90722d044
qhliew0302 <qhliew0302@gmail.com>
2021-04-26 18:09:34 +0800

    Create store.php

---
+++ store.php
@@ -0,0 +1,236 @@
+<?php
+    include_once "includes/connection.php";
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Store</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+ 
+        <link rel="stylesheet" type="text/css" media="screen" href="stylesheet.css">
+    </head>
+
+    <body>
+        <div class="header">
+            <a href="index.php" class= "nostyle">POWERPUFFGIRLS&BOYS</a>
+        </div>
+
+        <div class="bar">
+            <form action= "" method= "POST" style= 'display: inline;'>
+                <input type ="text" name= "search" placeholder="SEARCH BY ID">
+                <input type = "submit" name= "reset" value= "RESET" class="button"> 
+            </form>
+            <button id="insert" class= "button">INSERT</button>
+            <button id="update" class= "button">UPDATE</button>
+            <button id="delete" class= "button">DELETE</button>
+        </div>
+
+        <?php
+            echo "<table><thead><tr><th>Store_id</th><th>Address_id</th><th>Last_update</th></thead><tbody>"; 
+            if (isset($_POST['search'])){
+                
+                $search = $_POST['search'];
+            
+                $query = "SELECT * FROM store WHERE store_id LIKE '%$search%' ;";    
+                $result = mysqli_query($conn,$query);
+
+                if(mysqli_num_rows($result)>0){
+                    while($row = mysqli_fetch_assoc($result)){   
+                        echo "<tr><td>" . $row['store_id'] . "</td><td>" . $row['address_id'] . "</td><td>" . $row['last_update'] . "</td></tr>";  
+                    }
+                }else{
+                    echo "<tr><td> --NO DATA-- </td><td> --NO DATA-- </td><td> --NO DATA-- </td></tr>";
+                }
+
+            }elseif(isset($_POST['reset'])){
+                $query = "SELECT * FROM store;";
+                $result = mysqli_query($conn,$query);
+
+                while($row = mysqli_fetch_assoc($result)){   
+                    echo "<tr><td>" . $row['rental_id'] . "</td><td>" . $row['rental_date'] . "</td><td>" . $row['inventory_id'] . "</td>
+                    <td>" . $row['customer_id'] . "</td><td>" . $row['return_date'] . "</td><td>" . $row['staff_id'] . "</td><td>" . $row['last_update'] . "</td></tr>";  
+                }
+
+            }else {
+                $query = "SELECT * FROM store;";
+                $result = mysqli_query($conn,$query);
+
+                while($row = mysqli_fetch_assoc($result)){   
+                    echo "<tr><td>" . $row['store_id'] . "</td><td>" . $row['address_id'] . "</td><td>" . $row['last_update'] . "</td></tr>";  
+                }
+    
+            } 
+            echo "</tbody></table>";
+        ?>
+
+        <div class = "insert">
+            <div class = "popupcontent">
+                <div class = "insertdown" id="close">+</div>
+                <form action= "" method = "post">
+                    <p>Store ID:</p>
+                        <input type="text" name="storeid" onkeydown="return event.key != 'Enter'">
+                    <p>Address ID:</p>
+                        <input type="text" name="addressid" style="display:block;" onkeydown="return event.key != 'Enter'">
+                    <input type= "submit" name= "insert" class= "greenbutton" value ="INSERT">
+
+                    <?php
+                    if(isset($_POST['insert'])){
+                        if(!empty($_POST['storeid'])&& !empty($_POST['addressid'])){
+                            if($_POST['storeid'] > 0){
+                                $storeid= $_POST['storeid'];
+                                $query= "SELECT store_id FROM store WHERE store_id = $storeid;";
+                                $result= mysqli_query($conn,$query);
+
+                                if(mysqli_num_rows($result)==0){
+                                    $storeid= $_POST['storeid'];
+                                    $addressid= $_POST['addressid'];
+                                    $lastupdate= date('Y-m-d H:i:s');
+                                    $insert = "INSERT INTO store VALUES('$storeid','$addressid','$lastupdate');";
+                                    $result = mysqli_query($conn,$insert);
+                                    if ($result) {
+                                        echo '<script> alert("DATA INSERTED SUCCESSFULLY!")</script>';
+                                    }
+                                    else
+                                        echo '<script> alert("PREVIOUS INSERT FAILED! CHECK IF THERE WERE MISTAKES MADE WHEN INSERTING")</script>'; 
+
+                                    echo("<meta http-equiv='refresh' content='1'>");
+                                }
+                                else{
+                                    echo '<script> alert("PREVIOUS INSERT FAILED! CHECK IF THERE WERE MISTAKES MADE WHEN INSERTING")</script>';
+                                }  
+                            }
+                            else{
+                                echo '<script> alert("PREVIOUS INSERT FAILED! INVALID ID ENTERED")</script>';
+                            }
+                        } 
+                        else{
+                            echo '<script> alert("PREVIOUS INSERT FAILED, PLEASE FILL ALL FIELDS!")</script>';
+                        }
+                    }
+                    
+                    ?>
+                </form>
+            </div>
+        </div>
+
+        <div class = "update">
+            <div class = "popupcontent">
+                <div class = "updatedown" id="close">+</div>
+                <form action= "" method = "post">
+                <p>Store ID:</p>
+                        <input type="text" name="storeid" onkeydown="return event.key != 'Enter'">
+                    <p>Address ID:</p>
+                        <input type="text" name="addressid" style="display:block;" onkeydown="return event.key != 'Enter'">
+                    <input type= "submit" name= "update" class= "greenbutton" value ="UPDATE">
+
+                    <?php
+                    if(isset($_POST['update'])){
+                        if(!empty($_POST['storeid'])&& !empty($_POST['addressid'])){
+                            $storeid= $_POST['storeid'];
+                            $query= "SELECT store_id FROM store WHERE store_id = $storeid;";
+                            $result= mysqli_query($conn,$query);
+
+                            if(mysqli_num_rows($result)==1){
+                                $storeid= $_POST['storeid'];
+                                $addressid= $_POST['addressid'];
+                                $lastupdate= date('Y-m-d H:i:s');
+                                $update = "UPDATE store SET addressid= '$addressid',  last_update= '$lastupdate' WHERE store_id = $storeid;";
+                                $result = mysqli_query($conn,$update); 
+                                
+                                if($result)
+                                    echo '<script> alert("DATA UPDATED SUCCESSFULLY!")</script>';
+                                else
+                                    echo '<script> alert("PREVIOUS UPDATE FAILED! CHECK IF THERE WERE MISTAKES MADE WHEN UPDATING")</script>';
+                                
+                                echo("<meta http-equiv='refresh' content='1'>");
+                            }else{
+                                echo '<script> alert("PREVIOUS UPDATE FAILED! CHECK IF THERE WERE MISTAKES MADE WHEN UPDATING")</script>';
+                            }
+                                
+                        }else{
+                            echo '<script> alert("PREVIOUS UPDATE FAILED, PLEASE FILL ALL FIELDS!")</script>';
+                        }
+                    }
+                    ?>
+
+                </form>
+            </div>
+        </div>
+
+        <div class = "delete">
+            <div class = "popupcontent">
+                <div class = "deletedown" id="close">+</div>
+                <form action= "" method = "post">
+                    <p>Store ID:</p>
+                        <input type="text" name="storeid" onkeydown="return event.key != 'Enter'" style= "display:block">
+                    <input type= "submit" name= "delete" class= "greenbutton" value ="DELETE">
+                </form>
+            </div>
+
+            <?php
+                if(isset($_POST['delete'])){
+                    if(!empty($_POST['storeid'])){
+                        $storeid= $_POST['storeid'];
+                        $query= "SELECT store_id FROM store WHERE store_id = $storeid;";
+                        $result= mysqli_query($conn,$query);
+                            
+                        if(mysqli_num_rows($result)==1){
+                            $storeid= $_POST['storeid'];
+                            $delete = "DELETE FROM store WHERE store_id= '$storeid'; ";
+                            $result = mysqli_query($conn,$delete);
+                                
+                            if($result){
+                                echo '<script> alert("ROW DELETED SUCCESSFULLY!")</script>';
+                            }else{
+                                echo '<script> alert("DELETE FAILED! YOU ARE NOT ALLOWED TO DELETE THIS ROW")</script>';
+                            }
+                                
+                            echo("<meta http-equiv='refresh' content='1'>");
+                        }elseif (mysqli_num_rows($result)==0){
+                            echo '<script> alert("PREVIOUS DELETE FAILED! INVALID ID ENTERED")</script>';
+                        }
+                                
+                    }else{
+                        echo '<script> alert("PREVIOUS DELETE FAILED, PLEASE FILL ALL FIELDS!")</script>';
+                    }
+                }
+            ?>
+        </div>
+
+        <script>
+            document.getElementById('insert').addEventListener('click',
+            function(){
+                document.querySelector('.insert').style.display= 'flex';
+            }
+            );
+            document.querySelector('.insertdown').addEventListener('click',
+            function(){
+                document.querySelector('.insert').style.display= 'none';
+            });
+
+            document.getElementById('update').addEventListener('click',
+            function(){
+                document.querySelector('.update').style.display= 'flex';
+            }
+            );
+            document.querySelector('.updatedown').addEventListener('click',
+            function(){
+                document.querySelector('.update').style.display= 'none';
+            });
+
+            
+            document.getElementById('delete').addEventListener('click',
+            function(){
+                document.querySelector('.delete').style.display= 'flex';
+            }
+            );
+            document.querySelector('.deletedown').addEventListener('click',
+            function(){
+                document.querySelector('.delete').style.display= 'none';
+            });
+        </script>
+    </body>
+</html>
