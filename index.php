8c450ad2668986c2583617316b328806f877b5e1
aluxljy <aluxljy@gmail.com>
2021-04-27 14:39:05 +0800

    made changes to the menu

--- index.php
+++ index.php
@@ -13,34 +13,36 @@
             POWERPUFFGIRLS&BOYS
         </div>
         
-        <div class = "bar"><br /></div>
+        <div class = "bar">
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href= "actor.php">actor</a>
+                <a href= "address.php">address</a>
+                <a href= "category.php">category</a>
+                <a href= "city.php">city</a>
+                <a href= "country.php">country</a>
+                <a href= "customer.php">customer</a>
+                <a href= "film_actor.php">film_actor</a>
+                <a href= "film_category.php">film_category</a>
+                <a href= "film_text.php">film_text</a>
+                <a href= "film.php">film</a>
+                <a href= "inventory.php">inventory</a>
+                <a href= "language.php">language</a>
+                <a href= "payment.php">payment</a>
+                <a href= "rental.php">rental</a>
+                <a href= "staff.php">staff</a>
+                <a href= "store.php">store</a>
+                <a href= "payment&customer&staff.php">payment&customer&staff</a>
+                <a href= "film&film_text&film_category&category&language.php">film&film_text&film_category&category&language</a>
+                <a href= "country&city&address.php">country&city&address</a>
+            </div>
 
-        <div id="mySidenav" class="sidenav">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <a href= "actor.php">actor</a>
-            <a href= "address.php">address</a>
-            <a href= "category.php">category</a>
-            <a href= "city.php">city</a>
-            <a href= "country.php">country</a>
-            <a href= "customer.php">customer</a>
-            <a href= "film_actor.php">film_actor</a>
-            <a href= "film_category.php">film_category</a>
-            <a href= "film_text.php">film_text</a>
-            <a href= "film.php">film</a>
-            <a href= "inventory.php">inventory</a>
-            <a href= "language.php">language</a>
-            <a href= "payment.php">payment</a>
-            <a href= "rental.php">rental</a>
-            <a href= "staff.php">staff</a>
-            <a href= "store.php">store</a>
-            <a href= "payment&customer&staff.php">payment&customer&staff</a>
-            <a href= "film&film_text&film_category&category&language.php">film&film_text&film_category&category&language</a>
-            <a href= "country&city&address.php">country&city&address</a>
+            <div class="menu">
+                <span style="font-size:25px;cursor:pointer;color:white; text-align:left" onclick="openNav()">&#9776; MENU</span>
+            </div>
+            <br />
         </div>
 
-        <div class="menu">
-            <span style="font-size:25px;cursor:pointer;color:rgba(0, 176, 166, 1); text-align:left" onclick="openNav()">&#9776; MENU</span>
-        </div>
         
         <div>
             <h1>WELCOME TO OUR WEBSITE</h1>
