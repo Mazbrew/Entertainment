af46863b810375c93a59d55983cb461429c822c7
aluxljy <aluxljy@gmail.com>
2021-04-27 14:38:50 +0800

    actor 100% ok d wohoo

--- actor.php
+++ actor.php
@@ -19,8 +19,34 @@
         </div>
 
         <div class="bar">
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href= "actor.php">actor</a>
+                <a href= "address.php">address</a>
+                <a href= "category.php">category</a>
+                <a href= "city.php">city</a>
+                <a href= "country.php">country</a>
+                <a href= "customer.php">customer</a>
+                <a href= "film_actor.php">film_actor</a>
+                <a href= "film_category.php">film_category</a>
+                <a href= "film_text.php">film_text</a>
+                <a href= "film.php">film</a>
+                <a href= "inventory.php">inventory</a>
+                <a href= "language.php">language</a>
+                <a href= "payment.php">payment</a>
+                <a href= "rental.php">rental</a>
+                <a href= "staff.php">staff</a>
+                <a href= "store.php">store</a>
+                <a href= "payment&customer&staff.php">payment&customer&staff</a>
+                <a href= "film&film_text&film_category&category&language.php">film&film_text&film_category&category&language</a>
+                <a href= "country&city&address.php">country&city&address</a>
+            </div>
+
+            <div class="menu">
+                <span style="font-size:25px;cursor:pointer;color:white; text-align:left; display:inline;" onclick="openNav()">&#9776; MENU</span>
+            </div>
             <form action= "" method= "POST" style= 'display: inline;'>
-                <input type ="text" name= "search" placeholder="SEARCH BY ID" style= "border-radius: 5px;">
+                <input type ="text" name= "search" placeholder="SEARCH BY ACTOR ID" style= "border-radius: 5px;">
             </form>
             <form action= "" method= "POST" style= 'display: inline;'>
                 <input type = "submit" name= "reset" value= "RESET" class="button"> 
@@ -29,6 +55,7 @@
             <button id="update" class= "button">UPDATE</button>
             <button id="delete" class= "button">DELETE</button>
         </div>
+
 
         <?php
             echo "<table><thead><tr><th>Actor_id</th><th>First_name</th><th>Last_name</th><th>Last_update</th></thead><tbody>"; 
@@ -108,7 +135,7 @@
                                 }  
                             }
                             else{
-                                echo '<script> alert("PREVIOUS INSERT FAILED! INVALID ID ENTERED")</script>';
+                                echo '<script> alert("PREVIOUS INSERT FAILED! INVALID ACTOR ID ENTERED")</script>';
                             }
                         } 
                         else{
@@ -145,6 +172,8 @@
                                 $firstname= $_POST['firstname'];
                                 $lastname= $_POST['lastname'];
                                 $lastupdate= date('Y-m-d H:i:s');
+                                $firstname= strtoupper($firstname);
+                                $lastname= strtoupper($lastname);
                                 $update = "UPDATE actor SET first_name= '$firstname', last_name= '$lastname', last_update= '$lastupdate' WHERE actor_id = $actorid;";
                                 $result = mysqli_query($conn,$update); 
                                 
@@ -155,7 +184,7 @@
                                 
                                 echo("<meta http-equiv='refresh' content='1'>");
                             }else{
-                                echo '<script> alert("PREVIOUS UPDATE FAILED! CHECK IF THERE WERE MISTAKES MADE WHEN UPDATING")</script>';
+                                echo '<script> alert("PREVIOUS UPDATE FAILED! INVALID ACTOR ID ENTERED")</script>';
                             }
                                 
                         }else{
@@ -198,7 +227,7 @@
                                 
                             echo("<meta http-equiv='refresh' content='1'>");
                         }elseif (mysqli_num_rows($result)==0){
-                            echo '<script> alert("PREVIOUS DELETE FAILED! INVALID ID ENTERED")</script>';
+                            echo '<script> alert("PREVIOUS DELETE FAILED! INVALID ACTOR ID ENTERED")</script>';
                         }
                                 
                     }else{
@@ -239,6 +268,14 @@
             function(){
                 document.querySelector('.delete').style.display= 'none';
             });
+            
+            function openNav() {
+                document.getElementById("mySidenav").style.width = "620px";
+            }
+
+            function closeNav() {
+                document.getElementById("mySidenav").style.width = "0";
+            }
         </script>
     </body>
 </html>
